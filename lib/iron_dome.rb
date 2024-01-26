7e17959cdd9a57436b46d33756fa7cde8529ac54
Jose Augusto <joseaugusto.881@outlook.com>
2024-01-25 21:55:30 -0300

    adding basic functionality to read lock files and verify on osv base for vulns

--- lib/iron_dome.rb
+++ lib/iron_dome.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
 require_relative "iron_dome/version"
+require_relative "iron_dome/reader"
 
 module IronDome
   class Error < StandardError; end
-  # Your code goes here...
+
+  # class entry, this is the entrypoint of the gem.
+  class Entry
+    def main
+      Reader.new.call
+    end
+  end
 end
---
+++ lib/iron_dome/reader.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require "json"
+require "faraday"
+
+module IronDome
+  # read the lock file from the project
+  class Reader
+    URL = "https://api.osv.dev/v1/query"
+
+    def initialize; end
+
+    def call
+      read_file
+    end
+
+    private
+
+    def read_file
+      lock_files = Dir.glob(File.join("*.lock"))
+      lock_files.each do |file|
+        file_lines = File.read(file).lines
+        file_name = File.basename(file)
+        packages_and_versions = file_lines.flat_map { |line| line.scan(/\b(\w+) \(([\d.]+)\)/) }.to_h
+        result = osv_request(file_name, packages_and_versions)
+        puts(result)
+      end
+    end
+
+    def osv_request(_file_name, packages_and_versions)
+      conn = Faraday.new(URL) do |faraday|
+        faraday.adapter Faraday.default_adapter
+        faraday.headers["Content-Type"] = "application/json"
+        faraday.request :json
+      end
+
+      packages_and_versions.map do |package, version|
+        request_body = { version: version, package: { name: package } }
+
+        response = conn.post do |req|
+          req.url "/v1/query"
+          req.headers["Content-Type"] = "application/json"
+          req.body = request_body.to_json
+        end
+
+        JSON.parse(response.body)
+      end
+    end
+  end
+end
