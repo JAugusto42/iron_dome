2e9e916d3da89ace542198eae560dbf23655ccf9
Jose Augusto <joseaugusto.881@outlook.com>
2024-01-28 21:41:07 -0300

    basic spec and settings

--- spec/iron_dome_spec.rb
+++ spec/iron_dome_spec.rb
@@ -1,7 +1,32 @@
 # frozen_string_literal: true
 
 RSpec.describe IronDome do
+  let(:result) { IronDome::Entry.new.main }
+
+  let(:request) do
+    stub_request(:post, "https://api.osv.dev/v1/query")
+      .with(
+        body: { version: "2.8.6", package: { name: "addressable" } }.to_json,
+        headers: {
+          "Accept" => "*/*",
+          "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
+          "Content-Type" => "application/json",
+          "User-Agent" => "Faraday v2.9.0"
+        }
+      )
+      .to_return(status: 200, body: "", headers: {})
+  end
+
   it "has a version number" do
     expect(IronDome::VERSION).not_to be nil
+  end
+
+  describe ".Main" do
+    context "success" do
+      it "return an json object" do
+        request
+        expect(request.response.status.first).to eq(200)
+      end
+    end
   end
 end
