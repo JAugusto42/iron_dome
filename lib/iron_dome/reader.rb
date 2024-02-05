91dedd5a17d5ff3a44b3da5b6d9d634e89893547
Jose Augusto <joseaugusto.881@outlook.com>
2024-02-05 10:31:23 -0300

    fix rubocop offenses

--- lib/iron_dome/reader.rb
+++ lib/iron_dome/reader.rb
@@ -6,7 +6,7 @@ require "faraday"
 require_relative "requester"
 
 module IronDome
-  # read the lock file from the project
+  # The Reader class is responsible for reading lock files from the project and generating SARIF reports.
   class Reader
     URL = "https://api.osv.dev/v1/query"
 
@@ -19,67 +19,75 @@ module IronDome
     private
 
     def read_file
-      lock_files = Dir.glob(File.join("*.lock"))
-      result = []
-
-      lock_files.each do |file|
-        file_lines = File.read(file).lines
-        packages_and_versions = file_lines.flat_map { |line| line.scan(/\b(\w+) \(([\d.]+)\)/) }.to_h
-        result << Requester.osv_request(packages_and_versions)
-      end
+      lock_files = Dir.glob("*.lock")
+      result = lock_files.map { |file| process_lock_file(file) }
       result.first.compact
     end
 
-    def output_report_sarif
-      # output the scan result in a sarif file
-      sarif_json = convert_to_sarif(read_file)
+    def process_lock_file(file)
+      file_lines = File.read(file).lines
+      packages_and_versions = file_lines.flat_map { |line| line.scan(/\b(\w+) \(([\d.]+)\)/) }.to_h
+      Requester.osv_request(packages_and_versions)
+    end
 
-      File.open("result.sarif", "w") do |file|
-        file.puts sarif_json
-      end
+    def output_report_sarif
+      sarif_json = convert_to_sarif(read_file)
+      File.write("result.sarif", JSON.pretty_generate(sarif_json))
     end
 
     def convert_to_sarif(result)
-      sarif_result = {
+      sarif_result = sarif_schema
+      result.each { |vulnerability| process_vulnerability(sarif_result, vulnerability) }
+      JSON.pretty_generate(sarif_result)
+    end
+
+    def process_vulnerability(sarif_result, vulnerability)
+      vulnerability["vulns"].each do |vuln|
+        sarif_result[:runs][0][:results] << generate_sarif_result(vuln)
+      end
+    end
+
+    def generate_sarif_result(vuln)
+      {
+        ruleId: vuln["id"],
+        message: { text: vuln["summary"] },
+        locations: build_physical_location(vuln),
+        references: build_references(vuln)
+      }
+    end
+
+    def build_physical_location(vuln)
+      affected_package = vuln["affected"][0]["package"]
+      {
+        physicalLocation: {
+          artifactLocation: { uri: affected_package["purl"] },
+          region: { startLine: 1, startColumn: 1 }
+        }
+      }
+    end
+
+    def build_references(vuln)
+      vuln["references"].map { |ref| { type: "WEB", url: ref["url"] } }
+    end
+
+    def sarif_schema
+      {
         schema: "https://raw.githubusercontent.com/oasis-tcs/sarif-spec/master/Schemata/sarif-schema-2.1.0.json",
         version: "2.1.0",
-        runs: [
-          {
-            tool: {
-              driver: {
-                name: "OSv.dev API",
-                version: "1.0"
-              }
-            },
-            results: []
-          }
-        ]
+        runs: [build_run_info]
       }
+    end
 
-      result.each do |vulnerability|
-        vulnerability["vulns"].each do |vuln|
-          sarif_result[:runs][0][:results] << {
-            ruleId: vuln["id"],
-            message: {
-              text: vuln["summary"]
-            },
-            locations: [
-              physicalLocation: {
-                artifactLocation: {
-                  uri: vuln["affected"][0]["package"]["purl"]
-                },
-                region: {
-                  startLine: 1, # Adjust line number based on the actual information in your response
-                  startColumn: 1
-                }
-              }
-            ],
-            references: vuln["references"].map { |ref| { type: "WEB", url: ref["url"] } }
+    def build_run_info
+      {
+        tool: {
+          driver: {
+            name: "OSv.dev API",
+            version: "1.0"
           }
-        end
-      end
-
-      JSON.pretty_generate(sarif_result)
+        },
+        results: []
+      }
     end
   end
 end
