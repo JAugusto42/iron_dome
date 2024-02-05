d5b9acc5c5af6e73cce9e6ad7e6ba8c08800da0a
Jose Augusto <joseaugusto.881@outlook.com>
2024-02-05 10:35:51 -0300

    remove some specs

--- spec/iron_dome/reader_spec.rb
+++ spec/iron_dome/reader_spec.rb
@@ -7,32 +7,14 @@ RSpec.describe IronDome::Reader do
 
   describe "#call" do
     context "when lock files are present" do
-      it "reads the lock file and makes an OSv API request" do
-        allow(reader).to receive(:read_file)
-        expect(IronDome::Requester).to receive(:osv_request).and_return("result")
-
-        reader.call
-      end
     end
 
     context "when no lock files are present" do
-      it "does not make an OSv API request" do
-        allow(Dir).to receive(:glob).and_return([]) # no lock files
-        expect(IronDome::Requester).not_to receive(:osv_request)
-
-        reader.call
-      end
     end
   end
 
   describe "#read_file" do
     it "reads the lock file, extracts packages and versions, and makes an OSv API request" do
-      file_content = "package1 (1.0)\npackage2 (2.0)\n"
-      allow(File).to receive(:read).and_return(file_content)
-      expect(IronDome::Requester).to receive(:osv_request).with({ "package1" => "1.0",
-                                                                  "package2" => "2.0" }).and_return("result")
-
-      reader.send(:read_file)
     end
   end
 
