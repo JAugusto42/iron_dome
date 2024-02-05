1216f67d00d6d879556ca07e1a2137437ca76235
Jose Augusto <joseaugusto.881@outlook.com>
2024-02-05 09:52:08 -0300

    adding a method to return in a sarif spec

--- lib/iron_dome/reader.rb
+++ lib/iron_dome/reader.rb
@@ -13,19 +13,73 @@ module IronDome
     def initialize; end
 
     def call
-      read_file
+      output_report_sarif
     end
 
     private
 
     def read_file
       lock_files = Dir.glob(File.join("*.lock"))
+      result = []
+
       lock_files.each do |file|
         file_lines = File.read(file).lines
         packages_and_versions = file_lines.flat_map { |line| line.scan(/\b(\w+) \(([\d.]+)\)/) }.to_h
-        result = Requester.osv_request(packages_and_versions)
-        puts result
+        result << Requester.osv_request(packages_and_versions)
       end
+      result.first.compact
+    end
+
+    def output_report_sarif
+      # output the scan result in a sarif file
+      sarif_json = convert_to_sarif(read_file)
+
+      File.open('result.sarif', 'w') do |file|
+        file.puts sarif_json
+      end
+    end
+
+    def convert_to_sarif(result)
+      sarif_result = {
+        schema: 'https://raw.githubusercontent.com/oasis-tcs/sarif-spec/master/Schemata/sarif-schema-2.1.0.json',
+        version: '2.1.0',
+        runs: [
+          {
+            tool: {
+              driver: {
+                name: 'OSv.dev API',
+                version: '1.0'
+              }
+            },
+            results: []
+          }
+        ]
+      }
+
+      result.each do |vulnerability|
+        vulnerability['vulns'].each do |vuln|
+          sarif_result[:runs][0][:results] << {
+            ruleId: vuln['id'],
+            message: {
+              text: vuln['summary']
+            },
+            locations: [
+              physicalLocation: {
+                artifactLocation: {
+                  uri: vuln['affected'][0]['package']['purl']
+                },
+                region: {
+                  startLine: 1, # Adjust line number based on the actual information in your response
+                  startColumn: 1
+                }
+              }
+            ],
+            references: vuln['references'].map { |ref| { type: 'WEB', url: ref['url'] } }
+          }
+        end
+      end
+
+      return JSON.pretty_generate(sarif_result)
     end
   end
 end
---
+++ spec/iron_dome/requester_spec.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require "iron_dome/requester"
+require "json"
+
+RSpec.describe IronDome::Requester do
+  describe ".osv_request" do
+    let(:packages_and_versions) { { "sinatra" => "2.0.8", "rails" => "6.0.3" } }
+    let(:url) { "https://api.osv.dev/v1/query" }
+    let(:conn) { instance_double(Faraday::Connection) }
+    let(:response_body) { '{"some_key": "some_value"}' }
+    let(:json_response) { JSON.parse(response_body) }
+
+    before do
+      allow(Faraday).to receive(:new).with(url).and_return(conn)
+    end
+
+    context "when there is a valid response" do
+      it "makes a POST request to the OSV API with correct parameters" do
+        packages_and_versions.each do |package, version|
+          request_body = { version: version, package: { name: package } }
+
+          expect(conn).to receive(:post) do |&block|
+            request = double("Faraday::Request")
+            expect(request).to receive(:url).with("/v1/query")
+            expect(request).to receive(:headers).and_return({})
+            expect(request).to receive(:body=).with(request_body.to_json)
+            block.call(request)
+          end.and_return(double("Faraday::Response", body: response_body))
+        end
+
+        described_class.osv_request(packages_and_versions)
+      end
+
+      it "returns parsed JSON response" do
+        allow(conn).to receive(:post).and_return(double("Faraday::Response", body: response_body))
+        result = described_class.osv_request(packages_and_versions)
+        expect(result).to contain_exactly(json_response, json_response)
+      end
+    end
+
+    context "when there is an empty response body" do
+      it "returns nil" do
+        allow(conn).to receive(:post).and_return(double("Faraday::Response", body: "{}"))
+        result = described_class.osv_request(packages_and_versions)
+        expect(result).to contain_exactly(nil, nil)
+      end
+    end
+  end
+end
