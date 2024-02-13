27c9207d9449e371fbcc12329ce61a3e71e92403
Jose Augusto <joseaugusto.881@outlook.com>
2024-02-13 11:46:34 -0300

    adding output messages and a module for system output

---
+++ lib/iron_dome/output.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module IronDome
+  class Output
+
+  end
+end
--- lib/iron_dome/reader.rb
+++ lib/iron_dome/reader.rb
@@ -5,6 +5,7 @@ require "faraday"
 
 require_relative "requester"
 require_relative "sarif/output"
+require_relative "output"
 
 module IronDome
   # The Reader class is responsible for reading lock files from the project and generating SARIF reports.
@@ -20,6 +21,7 @@ module IronDome
     private
 
     def read_file
+      # read the lockfile, Gemfile.lock for now
       lock_files = Dir.glob("*.lock")
       lock_files.map { |file| process_lock_file(file) }
     end
@@ -27,14 +29,23 @@ module IronDome
     def process_lock_file(file)
       file_lines = File.read(file).lines
       packages_and_versions = file_lines.flat_map { |line| line.scan(/\b(\w+) \(([\d.]+)\)/) }.to_h
+      puts "Verifying vulnerabilities on osv database ..."
       result = Requester.osv_request(packages_and_versions)
       result.compact!
-      output_sarif_file_format(result)
+      system_output(result)
+      output_sarif_file_format(result) unless result # add options verification to do the output
     end
 
     def output_sarif_file_format(result)
-      IronDome::Sarif::Output.new.output_report(result) # add options verification to do the output
+      # method to call the module to generate the sarif report
+      puts "Generating the sarif output ..."
+      IronDome::Sarif::Output.new.output_report(result)
       puts ":: Sarif file outputed"
+    end
+
+    def system_output(result)
+      # method to call module to output the result on current shell.
+      puts "No vulnerabiities founded" if result.empty?
     end
   end
 end
