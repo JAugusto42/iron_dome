0a95ac96bd53af01335c06e566ac2b6c286a3b5b
Jose Augusto <joseaugusto.881@outlook.com>
2024-02-24 23:07:21 -0300

    refactor the specs

--- spec/iron_dome_spec.rb
+++ spec/iron_dome_spec.rb
@@ -1,21 +1,6 @@
 # frozen_string_literal: true
 
 RSpec.describe IronDome do
-  let(:body) { File.read("spec/files/requests_responses/success_response_with_vulns.json") }
-  let(:request) do
-    stub_request(:post, "https://api.osv.dev/v1/query")
-      .with(
-        body: { version: "2.8.6", package: { name: "addressable" } }.to_json,
-        headers: {
-          "Accept" => "*/*",
-          "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
-          "Content-Type" => "application/json",
-          "User-Agent" => "Faraday v2.9.0"
-        }
-      )
-      .to_return(status: 200, body: body, headers: {})
-  end
-
   it "has a version number" do
     expect(IronDome::VERSION).not_to be nil
   end
